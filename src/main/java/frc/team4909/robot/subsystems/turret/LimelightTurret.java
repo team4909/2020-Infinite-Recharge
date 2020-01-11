066edaa1f4bbf7f56dbe2cf2baea696108959e10
Jason Bishop <jasonbishop11@gmail.com>
2020-01-10 19:20:14 -0500

    Preliminary vision target tracking with turret
    Changed Limelight Turret to actually work.

--- src/main/java/frc/team4909/robot/subsystems/turret/LimelightTurret.java
+++ src/main/java/frc/team4909/robot/subsystems/turret/LimelightTurret.java
@@ -1,5 +1,5 @@
 package frc.team4909.robot.subsystems.turret;
-
+import java.text.DecimalFormat;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team4909.robot.Robot;
 import frc.team4909.robot.Vision;
@@ -7,9 +7,11 @@ import frc.team4909.robot.operator.controllers.BionicF310;
 
 public class LimelightTurret extends CommandBase{
     
-    private final double kP = 0.2;
-    private final double kD = 0.1;
+    private final double kP = 0.025;
+    private final double kD = 0.0005;
     private double lastError;
+    private double speed;
+    DecimalFormat twodec = new DecimalFormat("#.00");
 
     public LimelightTurret(final TurretSubsystem subsystem, final Vision vsubsystem){
         super();
@@ -24,13 +26,17 @@ public class LimelightTurret extends CommandBase{
 
     @Override
     public void execute(){
+        speed = (Robot.vision.getXOffset() * kP + (Robot.vision.getXOffset() - lastError) * kD);
         Robot.vision.updateVisionDashboard(); 
-        lastError = Robot.vision.getXOffset();
         if(Robot.driverGamepad.getRawButton(5))
         {
-            System.out.println(lastError);
-            Robot.turretsubsystem.setSpeed(Robot.vision.getXOffset() * kP + (Robot.vision.getXOffset() - lastError) * kD);
+            Robot.turretsubsystem.setSpeed(speed);
         }
-        Robot.turretsubsystem.setSpeed(Robot.driverGamepad.getThresholdAxis(BionicF310.RX)*0.2);
+        else
+        {
+            Robot.turretsubsystem.setSpeed(Robot.driverGamepad.getThresholdAxis(BionicF310.RX)*0.2);
+        }
+        System.out.println("" + kP + " " + twodec.format(speed) + " " + Robot.vision.getXOffset());
+        lastError = Robot.vision.getXOffset();
     }
 }
