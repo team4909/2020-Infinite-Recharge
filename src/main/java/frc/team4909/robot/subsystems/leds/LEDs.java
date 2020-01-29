429279dc2706c94de5d20545cd76a947b900c3ae
Jason Bishop <jasonbishop11@gmail.com>
2020-01-28 20:34:48 -0500

    Added LED Subsystem

---
+++ src/main/java/frc/team4909/robot/subsystems/leds/LEDs.java
@@ -0,0 +1,28 @@
+package frc.team4909.robot.subsystems.leds;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LEDs extends SubsystemBase{
+    public AddressableLED Strip;
+    public AddressableLEDBuffer ledBuffer;
+
+    public LEDs() {
+        Strip = new AddressableLED(0);
+        ledBuffer = new AddressableLEDBuffer(30);
+
+        Strip.setLength(ledBuffer.getLength());
+    }
+
+    public void setBlue() {
+        for (int i = 0; i <= ledBuffer.getLength(); i++) {
+            ledBuffer.setRGB(i, 0, 255, 0);
+        }
+        Strip.setData(ledBuffer);
+    }
+
+    public void initialize(){
+        setBlue();
+    }
+}
