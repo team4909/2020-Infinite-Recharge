e66a9e4a59748dfe7a15ddc25688bdc05c70bd3f
Jason Bishop <jasonbishop11@gmail.com>
2020-02-01 13:32:34 -0500

    Update ShooterSubsystem.java

--- src/main/java/frc/team4909/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/team4909/robot/subsystems/ShooterSubsystem.java
@@ -8,7 +8,6 @@ import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
