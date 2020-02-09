2bc91e339d79fac6cde376cbeee6939063073ee8
Jason Bishop <jasonbishop11@gmail.com>
2020-02-09 12:03:11 -0500

    Revert "Update Drive.java"
    This reverts commit 3516752c2eb526a515b33d4c491ef943d6bca1a4.

--- src/main/java/frc/team4909/robot/subsystems/drivetrain/Drive.java
+++ src/main/java/frc/team4909/robot/subsystems/drivetrain/Drive.java
@@ -1,41 +1,26 @@
-// // package frc.team4909.robot.subsystems.drivetrain;
+package frc.team4909.robot.subsystems.drivetrain;
 
-// // import edu.wpi.first.wpilibj2.command.CommandBase;
-// // import frc.team4909.robot.Robot;
-// // import frc.team4909.robot.operator.controllers.BionicF310;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.team4909.robot.Robot;
+import frc.team4909.robot.operator.controllers.BionicF310;
 
- public class Drive extends CommandBase{
+public class Drive extends CommandBase{
     
-     public Drive(DriveTrainSubsystem subsystem){
-         addRequirements(subsystem);
-     }
 
-      @Override
-      public void initialize(){
+    public Drive(DriveTrainSubsystem subsystem){
+        addRequirements(subsystem);
+    }
 
-      } 
+    @Override
+    public void initialize(){
 
-     @Override
-      public void execute(){
-        System.out.println("test2");
+    }
+
+    @Override
+    public void execute(){
         Robot.drivetrainsubsystem.arcadeDrive(
-        -Robot.driverGamepad.getThresholdAxis(BionicF310.LY),
-        Robot.driverGamepad.getThresholdAxis(BionicF310.RX)
+            -Robot.driverGamepad.getThresholdAxis(BionicF310.LY),
+            Robot.driverGamepad.getThresholdAxis(BionicF310.RX)
         );
-     }
- }
-// =======
-//     @Override
-//     public void initialize(){
-
-//     }
-
-//     @Override
-//     public void execute(){
-//         Robot.drivetrainsubsystem.arcadeDrive(
-//             -Robot.driverGamepad.getThresholdAxis(BionicF310.LY),
-//             Robot.driverGamepad.getThresholdAxis(BionicF310.RX)
-//         );
-//     }
-// }
-// >>>>>>> master
+    }
+}
--- src/main/java/frc/team4909/robot/subsystems/drivetrain/DriveTrainSubsystem.java
+++ src/main/java/frc/team4909/robot/subsystems/drivetrain/DriveTrainSubsystem.java
@@ -1,108 +1,56 @@
-// <<<<<<< I2C_Ultrasonic_Testing
-// // package frc.team4909.robot.subsystems.drivetrain;
+package frc.team4909.robot.subsystems.drivetrain;
 
-// // import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-// // import com.revrobotics.CANSparkMax;
-// // import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-// // import edu.wpi.first.wpilibj.SpeedControllerGroup;
-// // import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-// // import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-// // public class DriveTrainSubsystem extends SubsystemBase{
-// //     //WPI_TalonFX frontRight, frontLeft, backRight, backLeft;
-// //     CANSparkMax frontRight, frontLeft, backRight, backLeft;
-// //     SpeedControllerGroup m_right, m_left;
-// //     DifferentialDrive bionicDrive;
+public class DriveTrainSubsystem extends SubsystemBase{
+    WPI_TalonFX frontRight, frontLeft, backRight, backLeft;
+    //CANSparkMax frontRight, frontLeft, backRight, backLeft;
+    SpeedControllerGroup m_right, m_left;
+    DifferentialDrive bionicDrive;
 
-// //     public DriveTrainSubsystem() {
-// //         //frontRight = new WPI_TalonFX(0);
-// //         //backRight = new WPI_TalonFX(2);
-// //         frontRight = new CANSparkMax(1, MotorType.kBrushless);
-// //         backRight = new CANSparkMax(2, MotorType.kBrushless);
-// //         m_right = new SpeedControllerGroup(frontRight, backRight);
+    public DriveTrainSubsystem() {
+        frontRight = new WPI_TalonFX(1);
+        backRight = new WPI_TalonFX(2);
+        // frontRight = new CANSparkMax(1, MotorType.kBrushless);
+        // backRight = new CANSparkMax(2, MotorType.kBrushless);
+        m_right = new SpeedControllerGroup(frontRight, backRight);
 
-// //         //frontLeft = new WPI_TalonFX(1);
-// //         //backLeft = new WPI_TalonFX(3);
-// //         frontLeft = new CANSparkMax(3, MotorType.kBrushless);
-// //         backLeft = new CANSparkMax(4, MotorType.kBrushless);
-// //         m_left = new SpeedControllerGroup(frontLeft, backLeft);
+        frontLeft = new WPI_TalonFX(3);
+        backLeft = new WPI_TalonFX(4);
+        // frontLeft = new CANSparkMax(3, MotorType.kBrushless);
+        // backLeft = new CANSparkMax(4, MotorType.kBrushless);
+        m_left = new SpeedControllerGroup(frontLeft, backLeft);
 
-// //         bionicDrive = new DifferentialDrive(m_left, m_right);
-// //     }
+        bionicDrive = new DifferentialDrive(m_left, m_right);
+    }
 
-// //     public void arcadeDrive(double leftSpeed, double rightSpeed) {
-// //         double speedOutput = leftSpeed;
-// //         double turnOutput = rightSpeed;
+    public void arcadeDrive(double leftSpeed, double rightSpeed) {
+        double speedOutput = leftSpeed*0.5;
+        double turnOutput = rightSpeed*0.5;
 
-// //         bionicDrive.arcadeDrive(speedOutput, turnOutput);
-// //     }
+        bionicDrive.arcadeDrive(speedOutput, turnOutput);
+    }
 
-// //     public void initialize(){
-// //     }
+    public void tankDrive(double leftSpeed, double rightSpeed){
+        double leftOutput = leftSpeed;
+        double rightOutput = rightSpeed;
 
-// //     @Override
-// //     public void periodic(){
-// //         bionicDrive.feedWatchdog();
-// //     } 
-// //     DriveTrainSubsystem Jessica = new DriveTrainSubsystem();
+        bionicDrive.tankDrive(leftOutput, rightOutput);
+    }
 
-// // }
-// =======
-// package frc.team4909.robot.subsystems.drivetrain;
+    public void initialize(){
+    }
 
-// import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-// import com.revrobotics.CANSparkMax;
-// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-// import edu.wpi.first.wpilibj.SpeedControllerGroup;
-// import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-// public class DriveTrainSubsystem extends SubsystemBase{
-//     WPI_TalonFX frontRight, frontLeft, backRight, backLeft;
-//     //CANSparkMax frontRight, frontLeft, backRight, backLeft;
-//     SpeedControllerGroup m_right, m_left;
-//     DifferentialDrive bionicDrive;
-
-//     public DriveTrainSubsystem() {
-//         frontRight = new WPI_TalonFX(1);
-//         backRight = new WPI_TalonFX(2);
-//         // frontRight = new CANSparkMax(1, MotorType.kBrushless);
-//         // backRight = new CANSparkMax(2, MotorType.kBrushless);
-//         m_right = new SpeedControllerGroup(frontRight, backRight);
-
-//         frontLeft = new WPI_TalonFX(3);
-//         backLeft = new WPI_TalonFX(4);
-//         // frontLeft = new CANSparkMax(3, MotorType.kBrushless);
-//         // backLeft = new CANSparkMax(4, MotorType.kBrushless);
-//         m_left = new SpeedControllerGroup(frontLeft, backLeft);
-
-//         bionicDrive = new DifferentialDrive(m_left, m_right);
-//     }
-
-//     public void arcadeDrive(double leftSpeed, double rightSpeed) {
-//         double speedOutput = leftSpeed*0.5;
-//         double turnOutput = rightSpeed*0.5;
-
-//         bionicDrive.arcadeDrive(speedOutput, turnOutput);
-//     }
-
-//     public void tankDrive(double leftSpeed, double rightSpeed){
-//         double leftOutput = leftSpeed;
-//         double rightOutput = rightSpeed;
-
-//         bionicDrive.tankDrive(leftOutput, rightOutput);
-//     }
-
-//     public void initialize(){
-//     }
-
-//     @Override
-//     public void periodic(){
-//         bionicDrive.feedWatchdog();
-//     } 
+    @Override
+    public void periodic(){
+        bionicDrive.feedWatchdog();
+    } 
     
 
-// }
-// >>>>>>> master
+}
