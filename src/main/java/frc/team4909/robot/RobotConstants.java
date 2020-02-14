127107242c162f45b171418bcd0f7f1a59d77503
Jason Bishop <jasonbishop11@gmail.com>
2020-02-14 14:43:19 -0500

    Update RobotConstants.java

--- src/main/java/frc/team4909/robot/RobotConstants.java
+++ src/main/java/frc/team4909/robot/RobotConstants.java
@@ -9,9 +9,9 @@ public class RobotConstants{
     public static final double limelightPowerPortDiff = powerPortHeight-limelightHeight;
 
     //Shooter Constants
-    public static final double shooterkP = 5e-5; 
-    public static final double shooterkI = 8e-7;
-    public static final double shooterkD = 2e-4;
+    public static final double shooterkP = 0.005; 
+    public static final double shooterkI = 0;
+    public static final double shooterkD = 0;
 
     public static final double hoodkP = 12;
     public static final double hoodkI = 0;
