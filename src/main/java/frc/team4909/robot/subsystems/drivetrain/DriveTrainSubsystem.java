35d0eb282a94caae78626c23b07f70fb253fb908
Team4909 <team4909@gmail.com>
2020-02-24 18:00:11 -0500

    Gyro Based Driving

--- src/main/java/frc/team4909/robot/subsystems/drivetrain/DriveTrainSubsystem.java
+++ src/main/java/frc/team4909/robot/subsystems/drivetrain/DriveTrainSubsystem.java
@@ -2,13 +2,12 @@ package frc.team4909.robot.subsystems.drivetrain;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.team4909.robot.util.Util;
 
 public class DriveTrainSubsystem extends SubsystemBase{
     WPI_TalonFX frontRight, frontLeft, backRight, backLeft;
@@ -40,33 +39,24 @@ public class DriveTrainSubsystem extends SubsystemBase{
         bionicDrive = new DifferentialDrive(m_left, m_right);
     }
 
-/**
- * map a number from one range to another
- * @param  {num} value   the value to be mapped
- * @param  {num} old_min the minimum of value
- * @param  {num} old_max the maximum of value
- * @param  {num} new_min the new minimum value
- * @param  {num} new_max the new maximum value
- * @return The value remaped on the range [new_min new_max]
- */
-public double map(double value, double old_min, double old_max, double new_min, double new_max) {
-	return (value - old_min) / (old_max - old_min) * (new_max - new_min) + new_min;
-}
+
 
 
     public void arcadeDrive(double speed, double turn) {
 
         double speedOutput = speed;
+        double turnOutput = turn;
 
         // Since the robot doesn't move at speeds less than .3, this map function 
         // takes the full range of the joystick and converts it to the full range of the robot
         if (speed != 0) {
-            speedOutput = map(Math.abs(speed), 0.0, 1, .3, .75); 
+            speedOutput = Util.map(Math.abs(speed), 0.0, 1, .3, .75); 
             speedOutput = Math.copySign(speedOutput, speed);
-        }
+            }
 
-        double turnOutput = turn*0.65;//Math.pow(rightSpeed, 3);
+        // double turnOutput = turn*0.65;//Math.pow(rightSpeed, 3);
         SmartDashboard.putNumber("SpeedOutput", speedOutput);
+        SmartDashboard.putNumber("TurnOutoput", turnOutput);
 
         if(inverted){
             speedOutput = speedOutput*-1;
@@ -84,6 +74,11 @@ public double map(double value, double old_min, double old_max, double new_min, 
         bionicDrive.tankDrive(leftOutput, rightOutput);
     }
 
+    public void drivetrainPID(){
+        
+        
+
+}
     public void invertDriveDirection(){
         inverted = !inverted;
     }
