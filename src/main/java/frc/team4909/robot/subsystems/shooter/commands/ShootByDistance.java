a366fe16076bc10732740ccf9dc4efd04f1ea2a6
Mihir <>
2020-02-24 14:51:17 -0500

    Update ShootByDistance.java

--- src/main/java/frc/team4909/robot/subsystems/shooter/commands/ShootByDistance.java
+++ src/main/java/frc/team4909/robot/subsystems/shooter/commands/ShootByDistance.java
@@ -3,7 +3,6 @@ package frc.team4909.robot.subsystems.shooter.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team4909.robot.Robot;
 import frc.team4909.robot.RobotConstants;
-import frc.team4909.robot.Vision;
 
 public class ShootByDistance extends CommandBase {
 
