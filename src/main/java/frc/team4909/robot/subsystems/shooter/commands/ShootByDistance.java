4095643d11cccff3f6ebc04cb9f3f601a63de1d4
Mihir <>
2020-02-24 16:16:46 -0500

    Comments added

--- src/main/java/frc/team4909/robot/subsystems/shooter/commands/ShootByDistance.java
+++ src/main/java/frc/team4909/robot/subsystems/shooter/commands/ShootByDistance.java
@@ -4,8 +4,13 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team4909.robot.Robot;
 import frc.team4909.robot.RobotConstants;
 
+//Executed from the ParallelCommandGroup FollowAndAim.
 public class ShootByDistance extends CommandBase {
 
+    /**
+     * Values obtained through 6 tested points: 
+     * (Distance to Goal, Hood Angle Reqired to Make The Inner Port).
+     */
     private double a = RobotConstants.hoodCofA;
     private double b = RobotConstants.hoodCofB;
     private double c = RobotConstants.hoodCofC;
@@ -17,15 +22,20 @@ public class ShootByDistance extends CommandBase {
 
     @Override
     public void initialize() {
-        Robot.hoodSubsystem.setHoodAngle(calculateAngle(Robot.vision.calculateDistanceFromCameraHeight(
+        Robot.hoodSubsystem.setHoodAngle(calculateAngle((int)Robot.vision.calculateDistanceFromCameraHeight(
                 RobotConstants.powerPortHeight, RobotConstants.limelightHeight, RobotConstants.limelightAngle)));
     }
 
-    private int calculateAngle(double distance) {
+    /**
+     * @param distance The distance from the Limelight to the Goal.
+     * @return The angle of the quadratic
+     */
+    private int calculateAngle(int distance) {
+        // Use previously tested coefficients to calculate angle
         double outputAngle;
         double firstMono = a * Math.pow(distance, 2);
         double secondMono = b * distance;
-        y = firstMono + secondMono + c;
+        outputAngle = firstMono + secondMono + c;
         return (int) outputAngle;
     }
 }
