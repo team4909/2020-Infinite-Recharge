4fce93f4b709220d5d79f2df2d283cff48877a6a
Team4909 <team4909@gmail.com>
2020-02-26 17:54:54 -0500

    Explanation Code? :neutral_face:
    No way....

--- src/main/java/frc/team4909/robot/subsystems/leds/SetGreen.java
+++ src/main/java/frc/team4909/robot/subsystems/leds/SetGreen.java
@@ -3,6 +3,12 @@ package frc.team4909.robot.subsystems.leds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team4909.robot.Robot;
 
+/**
+ * Sets the LEDs to Green through a command. This is because the setting to
+ * Green will happen through a button press. It is much easier to call a Command
+ * over a ButtonPress, rather than calling a method of a subsystem. It's easier
+ * than calling the method SetGreen() directly from the LED subsystem.
+ */
 public class SetGreen extends CommandBase {
 
     public SetGreen() {
