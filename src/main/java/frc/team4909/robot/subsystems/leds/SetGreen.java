f27266af074680637cc362a773a31a67b76d65cd
Team4909 <team4909@gmail.com>
2020-02-26 17:57:16 -0500

    SetGreen through command :green_heart: :anguished:

--- src/main/java/frc/team4909/robot/subsystems/leds/SetGreen.java
+++ src/main/java/frc/team4909/robot/subsystems/leds/SetGreen.java
@@ -7,7 +7,7 @@ import frc.team4909.robot.Robot;
  * Sets the LEDs to Green through a command. This is because the setting to
  * Green will happen through a button press. It is much easier to call a Command
  * over a ButtonPress, rather than calling a method of a subsystem. It's easier
- * than calling the method SetGreen() directly from the LED subsystem.
+ * than calling the method SetGreen() directly from the 'LEDs' subsystem.
  */
 public class SetGreen extends CommandBase {
 
