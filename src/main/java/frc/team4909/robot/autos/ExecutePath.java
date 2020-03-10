9d8904e12f25a5180b0e77c3ac46973ed390247c
Mihir <>
2020-03-10 19:19:37 -0400

    a :grin:

---
+++ src/main/java/frc/team4909/robot/autos/ExecutePath.java
@@ -0,0 +1,9 @@
+package frc.team4909.robot.autos;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ExecutePath extends CommandBase {
+
+    private final String PATH1 = "paths/Path";
+    
+}
