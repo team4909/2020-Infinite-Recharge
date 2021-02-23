b94fba5ea54b1b0dfb9fa273b82d7d582fad3f45
Saketh Mynampati <smynampati42@gmail.com>
2021-02-22 20:45:11 -0500

    added turnRobot.java

--- src/main/java/frc/team4909/robot/subsystems/drivetrain/commands/turnRobot.java
+++ src/main/java/frc/team4909/robot/subsystems/drivetrain/commands/turnRobot.java
@@ -1,52 +1,66 @@
 package frc.team4909.robot.subsystems.drivetrain.commands;
 
-import edu.wpi.first.wpilibj2.command.*;
-import com.kauailabs.navx.frc.*;
-import edu.wpi.first.wpilibj.*;
-import frc.team4909.robot.subsystems.drivetrain.*;
-import frc.team4909.robot.*;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.team4909.robot.Robot;
+import frc.team4909.robot.RobotConstants;
 
 public class TurnRobot extends CommandBase{
     //error is how much we have to turn
     //gain is the speed to turn at based on error
     public double error;
     public double targetState;
-    public double GAIN = 0.3; //TODO check value | Gain is the speed at which the robot will spin
-    public double SPEED = 0.5; //TODO check value
-    public double degrees;
+    public PIDController turnRobot;
+    public final double SPEED = 0; //TODO check value
+    public int numLoops = 0;
 
     public TurnRobot(double degrees){
         super();
         addRequirements(Robot.drivetrainsubsystem);
-        this.degrees = degrees;
-    }
-
-    @Override
-    public void initialize() {
+        System.out.println("TURN ROBOT IS CONSTUCTED");
+        turnRobot = new PIDController(RobotConstants.TURN_KP, RobotConstants.TURN_KI, RobotConstants.TURN_KD);
+        //TODO change the format of final constants to CAPS_AND_UNDERSCORES.
         targetState = degrees + Robot.navX.getAngle(); //Gets the abosolute position | TODO Find what get angle really means / gives back
-        System.out.println(Robot.navX.getAngle());
-        error = targetState - Robot.navX.getAngle(); //Gets the absolute error
-        Robot.drivetrainsubsystem.arcadeDrive(SPEED, error * GAIN);
+        
     }
-
-
     
     @Override
-    public void execute(){
-        error = targetState - Robot.navX.getAngle();
-        Robot.drivetrainsubsystem.arcadeDrive(SPEED, error * GAIN);
+    public void initialize() {
+        super.initialize();
+        turnRobot.setSetpoint(targetState);
+        // turnRobot.setTolerance(3);//TODO Test Value and figure out units
+        System.out.println("TURN ROBOT IS INITILIZED");
+    }
+
+    @Override
+    public void execute() {
+        //error = targetState - Robot.navX.getAngle();
+        //System.out.println("Error:" + error);
+        //double output = error * GAIN;
+        double output = turnRobot.calculate(Robot.navX.getAngle());
+        //Robot.drivetrainsubsystem.tankDrive(output, -output);
+        Robot.drivetrainsubsystem.tankDrive(output, -output); //TODO where does the negative go???
+        if(++numLoops == 10){
+            System.out.println("Current Angle: " + Robot.navX.getAngle());
+            System.out.println("Target Angle: " + targetState);
+            System.out.println("PID Output: " + turnRobot.calculate(Robot.navX.getAngle()));
+            numLoops = 0;
+        }
     }
 
     @Override
     public boolean isFinished() {
-        if(error < 5){ //TODO test value
+        if(turnRobot.getPositionError() < 1){ 
+            System.out.println("Error is less than 3 units");
             return true;
         }
+        
         return false;
     }
 
     @Override
     public void end(boolean interrupted){
+        System.out.println("Robot should be stopped");
         Robot.drivetrainsubsystem.arcadeDrive(0, 0);
     }
     
