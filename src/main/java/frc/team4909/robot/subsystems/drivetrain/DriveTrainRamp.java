bd6aad7c5c26513a45845759ffcc689e2174b291
Saketh Mynampati <smynampati42@gmail.com>
2021-02-27 19:10:09 -0500

    Fixed Drive Ramping Acceleration-only function

--- src/main/java/frc/team4909/robot/subsystems/drivetrain/DriveTrainRamp.java
+++ src/main/java/frc/team4909/robot/subsystems/drivetrain/DriveTrainRamp.java
@@ -14,40 +14,61 @@ public class DriveTrainRamp {
     //Class Variables :
    //The maximum rate of change of the "speedoutput" in the drivetrain arcade drive.  
     private static double accelThreshold = 0.03; //TODO test value
-    private static double lastValue = 0; 
+    private static double lastSetSpeed = 0; 
 
-    public static double getRampedOutput(double speedOutput){
+    public static double getRampedOutput(double targetSpeed){
 
         //TODO Functionize the below line.
-        accelThreshold = 0.03 - Math.abs(lastValue) * 0.015;
+        accelThreshold = 0.03 - Math.abs(lastSetSpeed) * 0.015;
         //If the speed is trying to acellerate too fast
-        if(Math.abs(speedOutput - lastValue) > accelThreshold){
+        if(Math.abs(targetSpeed - lastSetSpeed) > accelThreshold){
             //Sets speedOutPut equal to its last value plus the limit 
-            speedOutput = lastValue + Math.copySign(accelThreshold, speedOutput - lastValue); //CopySign handles accel and deccel
+            targetSpeed = lastSetSpeed + Math.copySign(accelThreshold, targetSpeed - lastSetSpeed); //CopySign handles accel and deccel
         }
 
-        lastValue = speedOutput;
-        return speedOutput;
+        lastSetSpeed = targetSpeed;
+        return targetSpeed;
 
     }
     
-    public static double getAccelerationRampedOutput(double speedOutput){
+    //OLD VERSION OF METHOD BELOW
+    
+    public static double getAccelerationRampedOutputOld(double targetSpeed){
         //TODO Functionize the below line.
         accelThreshold = 0.02;
         //If the speed is trying to acellerate too fast
-        if((speedOutput - lastValue) < -accelThreshold){
+        if( (targetSpeed - lastSetSpeed) < (-1 * accelThreshold) ){
             //Sets speedOutPut equal to its last value plus the limit 
-            speedOutput = lastValue + Math.copySign(accelThreshold, speedOutput - lastValue); //CopySign handles accel and deccel
+            targetSpeed = lastSetSpeed + Math.copySign(accelThreshold, targetSpeed - lastSetSpeed); //CopySign handles accel and deccel
         }
 
-        lastValue = speedOutput;
-        return speedOutput;
+        lastSetSpeed = targetSpeed;
+
+        return targetSpeed;
 
     }
 
+    public static double getAccelerationRampedOutput(double targetSpeed){
+        //TODO Functionize the below line.
+        accelThreshold = 0.02;
+        //Checks if the speed is trying to increase, and by too much. Misses the case where the speed isn't changing, but the direction is.
+        if( Math.abs(targetSpeed) > Math.abs(lastSetSpeed) && Math.abs(targetSpeed - lastSetSpeed) > accelThreshold) {
+            //Sets the new speed equal to its last value plus the limit. 
+            targetSpeed = lastSetSpeed + Math.copySign(accelThreshold, targetSpeed - lastSetSpeed);
+        } else if (targetSpeed * lastSetSpeed < 0 && Math.abs(targetSpeed - lastSetSpeed) > accelThreshold) {
+            targetSpeed = 0; //CopySign handles accel
+        }
+
+        lastSetSpeed = targetSpeed;
+
+        return targetSpeed;
+    }
 
     public static void zeroLastValue() {
-        lastValue = 0;
+        lastSetSpeed = 0;
     }
     
+
+    
+
 }
