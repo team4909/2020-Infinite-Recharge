916052976f951138b473d8bbb6830ac9a6a5dde1
team4909 <team4909@gmail.com>
2021-11-04 17:12:10 -0400

    autos?

--- src/main/java/frc/team4909/robot/autos/AutoShootThreePickUpThree.java
+++ src/main/java/frc/team4909/robot/autos/AutoShootThreePickUpThree.java
@@ -1,6 +1,5 @@
 package frc.team4909.robot.autos;
 
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.team4909.robot.subsystems.drivetrain.commands.Drive;
@@ -11,14 +10,14 @@ import frc.team4909.robot.subsystems.indexer.commands.SmartIndexerAndSorterUp;
 public class AutoShootThreePickUpThree extends SequentialCommandGroup{
     public AutoShootThreePickUpThree(){
         addCommands(
-                // new ShootThree(),
-                // new ParallelDeadlineGroup(
-                    new Drive(100)
-                //     // ,new SmartIndexerAndSorterUp()
-                // ),
-                ,new WaitCommand(5)
-                ,new Drive(-100)
-                // new ShootThree()
+                new ShootThree(),
+                new ParallelDeadlineGroup(
+                    new Drive(185),
+                    new SmartIndexerAndSorterUp()
+                ),
+                new Drive(-125),
+                new Drive(21),
+                new ShootThree()
         
         );
     }
